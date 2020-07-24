2707e89c2367d4967ef0c3deeea8b447a948bf4c
BEB <boufcoulman@hotmail.fr>
2020-07-24 10:42:06 +0200

    :construction: fermeture dofus + plein ecran

--- scripts/dofus_window.py
+++ scripts/dofus_window.py
@@ -6,8 +6,8 @@ import time
 def use_key(typing_device, keyboard_key):
     """
     Simule un appui bref sur une touche
-    Pour une obscure raison, la fonction keyboard.Controller.tap n'est pas
-    présente dans le paquet pynput de ma machine, autrement
+    Pour une obscure raison, la fonction pynput.keyboard.Controller.tap n'est
+    pas présente dans le paquet pynput de ma machine, autrement
     il ferait très bien le travail
     """
     typing_device.press(keyboard_key)
@@ -39,6 +39,10 @@ def open_dofus():
     use_key(typing_device, keyboard.Key.enter)
 
     time.sleep(10)
+    # Vérification plein écran windows+arrow up
+    with typing_device.pressed(keyboard.Key.cmd):
+        use_key(typing_device, keyboard.Key.up)
+
     # Passage de la popup "lancement sans launcher"
     use_key(typing_device, keyboard.Key.enter)
 
@@ -46,6 +50,23 @@ def open_dofus():
     # Saisie du mot de passe
     # typing_device.type()
 
+    # Pour fermer :
+    # Key.alt_l + Key.f4
+
+
+def altf4():
+    """
+    Réalise un appui d'alt_l + f4 dans le but de
+    fermer la fenêtre windows active
+    """
+    # Ajout du clavier
+    typing_device = keyboard.Controller()
+    # Appui alt+f4
+    with typing_device.pressed(keyboard.Key.alt_l):
+        use_key(typing_device, keyboard.Key.f4)
+
 
 if __name__ == "__main__":
     open_dofus()
+    time.sleep(5)
+    # altf4()
--- test_keyboard.py
+++ test_keyboard.py
@@ -1,7 +1,6 @@
 from pynput import mouse, keyboard
 from scripts.stopper import escape_on_escape
 from scripts.dofus_window import use_key
-import env_var
 import time
 
 escape_on_escape()
@@ -14,5 +13,3 @@ time.sleep(0.8)
 # Appel de windows+R
 with typing_device.pressed(keyboard.Key.cmd):
     use_key(typing_device, 'r')
-
-typing_device.type(PASSWORD)
