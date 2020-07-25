c0b8f23c45464f3cc795da09328596532e34ab6e
BEB <boufcoulman@hotmail.fr>
2020-07-25 14:02:49 +0200

    :construction: lecture password

--- scripts/read_config.py
+++ scripts/read_config.py
@@ -3,8 +3,19 @@ var_path = "config_vars.toml"
 
 
 def password():
+    """
+    Retourne le mot de passe du fichier de configuration
+    """
     config_vars = toml.load(var_path)
-    print(config_vars['screen_infos']['test'])
+    return config_vars['login_infos']['password']
+
+
+def screen_infos():
+    """
+    Retourne les informations concernant l'écran du fichier de configuration
+    """
+    config_vars = toml.load(var_path)
+    return config_vars['screen_infos']
 
 
 if __name__ == "__main__":
